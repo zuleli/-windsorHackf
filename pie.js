8ce56b54d7306140354b27bd33e0d4fdcabef975
Zule Li <zule.li@hotmail.com>
2015-03-07 19:24:16 -0500

    Update pie.js

--- pie.js
+++ pie.js
@@ -3,6 +3,8 @@
  almond 0.3.0 Copyright (c) 2011-2014, The Dojo Foundation All Rights Reserved.
  Available via the MIT or new BSD license.
  see: http://github.com/jrburke/almond for details
+ 
+ Modified by Zule Li
 */
 'use strict';
 var requirejs, require, define;
